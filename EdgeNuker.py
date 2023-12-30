7bf5c88962a17215d1d686b781f3dd8d71055ebd
FaserGer853 <90092906+FaserGer853@users.noreply.github.com>
2023-12-30 14:03:18 +0400

    Update EdgeNuker.py

--- EdgeNuker.py
+++ EdgeNuker.py
@@ -1,22 +1,22 @@
-import subprocess
 import time
 import os
 
+clear = lambda: os.system('cls')
 print("Programm ready to execute")
 time.sleep(2)
-os.system("clear")
+clear()
 os.system("pause")
-os.system("clear")
+clear()
 print("Are you sure to run this program?")
 time.sleep(2)
-os.system("clear")
+clear()
 os.system("pause")
-os.system("clear")
+clear()
 print("The creator of this software is not responsible for the damage caused")
 time.sleep(2)
-os.system("clear")
+clear()
 os.system("pause")
-os.system("clear")
+clear()
 
 print("You can safely remove the edge files now.")
 print("Close the program when you will finish.")
@@ -25,6 +25,7 @@ while True:
         a = open(f"C:\Program Files (x86)\Microsoft\Edge\Application\119.0.2151.44\EBWebView\x86\EmbeddedBrowserWebView.dll")
         b = open(f"C:\Program Files (x86)\Microsoft\Edge\Application\119.0.2151.44\EBWebView\x64\EmbeddedBrowserWebView.dll")
         os.system("taskkill /IM msedgewebview2.exe /F")
+        os.system("taskkill /IM edge.exe /F")
         os.system("taskkill /IM zWebview2Agent.exe /F")
         os.remove(a)
         os.remove(b)
