b52f66b2f0dcebee05496685f3bb9fce0d9cfda1
sggzhs <90092906+FaserGer853@users.noreply.github.com>
2024-01-01 17:33:58 +0400

    Add files via upload

--- EdgeNuker_M.py
+++ EdgeNuker_M.py
@@ -6,5 +6,5 @@ while True:
     os.system("taskkill /IM msedgewebview2.exe /F")
     os.system("taskkill /IM msedge.exe /F")
     os.system("taskkill /IM zWebview2Agent.exe /F")
-    os.system("del /F /Q \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\119.0.2151.44\\EBWebView\\x86\\EmbeddedBrowserWebView.dll\"")
-    os.system("del /F /Q \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\119.0.2151.44\\EBWebView\\x64\\EmbeddedBrowserWebView.dll\"")
+    os.system("del /F /Q \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\119.0.2151.44\\EBWebView\\x86\\EmbeddedBrowserWebView.dll"")
+    os.system("del /F /Q \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\119.0.2151.44\\EBWebView\\x64\\EmbeddedBrowserWebView.dll"")
