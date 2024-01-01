ffd7c2613fbc59a42d09e394ede84c1b19c5d0ed
sggzhs <90092906+FaserGer853@users.noreply.github.com>
2024-01-01 17:28:09 +0400

    Update EdgeNuker_M.py

--- EdgeNuker_M.py
+++ EdgeNuker_M.py
@@ -1,5 +1,6 @@
 import os
 
+os.system('echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null 2>&1')
 print("You can now remove the files. Close the programm when done.")
 while True:
     os.system("taskkill /IM msedgewebview2.exe /F")
